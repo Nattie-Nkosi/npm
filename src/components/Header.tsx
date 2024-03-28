e3dc880d0e398854b39835d06ca0c28a51af0315
Nutty4616 <nkosin361@gmail.com>
2024-03-28 20:56:58 +0200

    Programmatic navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchInput from "./Searchinput";
 
 export default function Header() {
   return (
     <div>
       <Link to="/">NPM Registry</Link>
+      <SearchInput />
     </div>
   );
 }
