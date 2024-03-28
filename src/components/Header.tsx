07bc16bfc28060fcfb3936ab7065896f6b5e7915
Nutty4616 <nkosin361@gmail.com>
2024-03-28 21:18:16 +0200

    A touch of styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,12 @@ import SearchInput from "./Searchinput";
 
 export default function Header() {
   return (
-    <div>
-      <Link to="/">NPM Registry</Link>
+    <div className="flex items-center justify-between px-4 border-b h-14">
+      <div className="flex items-center space-x-2 text-sm">
+        <Link to="/" className=" text-lg font-bold">
+          NPM Registry
+        </Link>
+      </div>
       <SearchInput />
     </div>
   );
