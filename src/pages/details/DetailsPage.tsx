a1339227ebf93895d57dacd7c431d224aa6fea70
Nattie-Nkosi <nkosin361@gmail.com>
2024-04-19 17:55:10 +0200

    Typing the Page and Loader Together

--- src/pages/details/DetailsPage.tsx
+++ src/pages/details/DetailsPage.tsx
@@ -1,3 +1,12 @@
+import { useLoaderData } from "react-router-dom";
+import { DetailsLoaderResult } from "./detailsLoader";
+
 export default function DetailsPage() {
-  return <div>Details page</div>;
+  const { details } = useLoaderData() as DetailsLoaderResult;
+
+  return (
+    <div>
+      {details.name} - {details.description}
+    </div>
+  );
 }
--- src/pages/details/detailsLoader.ts
+++ src/pages/details/detailsLoader.ts
@@ -1,11 +1,16 @@
 import { Params } from "react-router-dom"
 import { getPackage } from "../../api/queries/getPackage";
+import { PackageDetails } from "../../api/types/packageDetails";
 
 interface LoaderArgs {
   params: Params
 }
 
-export default async function detailsLoader({ params }: LoaderArgs) {
+export interface DetailsLoaderResult {
+  details: PackageDetails
+}
+
+export default async function detailsLoader({ params }: LoaderArgs): Promise<DetailsLoaderResult> {
   const { name } = params;
 
   if (!name) {
